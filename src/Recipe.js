8cdde7a73bbe2c5ba632a20a0dec16926c47f61f
Victor Gambier <vgambier@excilys.com>
2020-08-05 11:59:41 +0200

    Changed the way we access mock recipes

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import './Recipe.css';
 
 function Recipe(props) {
+
+  const [recipe, setRecipe] = useState(props.recipe);  // We can now access a recipe by just typing "recipe" instead of "props.recipe"
+  const { picture, name, description } = recipe; // We can now access attributes by just typing "picture" instead of "recipe.picture"
+
   return (
 
     <div className="Recipe">
 
       <Card style={{ width: '18rem' }}>
-        <CardImg variant="top" src={props.recipe.picture} />
+        <CardImg variant="top" src={picture} />
         <CardBody>
-          <CardTitle>{props.recipe.name}</CardTitle>
-          <CardText>{props.recipe.description}</CardText>
+          <CardTitle>{name}</CardTitle>
+          <CardText>{description}</CardText>
         </CardBody>
       </Card>
 
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MOCK } from './Mock';
 import Recipe from './Recipe';
+import './RecipeList.css';
 
 function RecipeList() {
     return (
